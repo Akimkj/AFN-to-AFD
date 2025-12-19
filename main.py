aaaf668a892cab776897f1d144a14affa69ef59f
Mika <kakamarques.0202@gmail.com>
2025-12-18 23:39:57 -0300

    feat: atualizando o arquivo main para executar todos os arquivos src/ na ordem, conforme solicitado

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
 from src.conversor_afd import afn_to_afd
 from src.tabela import imprimir_tabela_afd
 from src.visualizacao import visualizar_afd
-
-afn_teste = {
-    "estados": ["A", "B", "C", "D"],
-    "simbolos": ["0", "1"],
-    "inicial": "A",
-    "finais": ["D"],
-    "transicoes": {
-        ("A", "0"): "B",
-        ("A", "1"): "A",
-        ("A", "0"): "A",
-        ("B", "0"): "C",
-        ("B", "1"): "B",
-        ("C", "0"): "D",
-        ("C", "1"): "C",
-        ("D", "0"): "D",
-        ("D", "1"): "D"
-    }
-}
-
-afd_teste = afn_to_afd(afn_teste)
-imprimir_tabela_afd(afd_teste)
+from src.inputBusca import inputSearch
 
 
-visualizar_afd(afd_teste)
+afn = inputSearch('inputAFN.txt')
+afd = afn_to_afd(afn)
+imprimir_tabela_afd(afd)
+visualizar_afd(afd)
+
 
 
 
